2baf8b96521e93bb5ad225cb0cd192c3509791f8
samrad29 <59303974+samrad29@users.noreply.github.com>
2024-09-09 11:01:50 -0500

    Add files via upload

---
+++ db_check.py
@@ -0,0 +1,13 @@
+
+def create_record(c, conn, threadId, date, sender):
+    c.execute("INSERT INTO sent_emails (threadId, emailDate, postUser) VALUES (?,?,?)", (threadId,date,sender))
+    conn.commit()
+
+def check_duplicate(c, msg):
+# check if the email thread has alreaby been processed
+    threadID = msg['threadId']
+    c.execute('SELECT 1 from sent_emails where threadId = ?', (threadID,))
+    if c.fetchone():
+        return 1 #return 1 if this is a duplicate
+    else:
+        return 0 #return 0 if this is a new lead
